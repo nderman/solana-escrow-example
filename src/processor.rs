65035593db41a583b804051a156e6965b96aff65
Neal Derman <nderman@gmail.com>
2021-12-08 14:20:46 +0200

    update

--- src/processor.rs
+++ src/processor.rs
@@ -6,6 +6,7 @@ use solana_program::{
   msg,
   pubkey::Pubkey,
   sysvar::{rent::Rent, Sysvar},
+  program::invoke
 };
 
 use crate::{instruction::EscrowInstruction, error::EscrowError, state::Escrow};
@@ -61,6 +62,25 @@ impl Processor {
     Escrow::pack(escrow_info, &mut escrow_account.try_borrow_mut_data()?)?;
     let (pda, _bump_seed) = Pubkey::find_program_address(&[b"escrow"], program_id);
 
+    let token_program = next_account_info(account_info_iter)?;
+    let owner_change_ix = spl_token::instruction::set_authority(
+      token_program.key,
+      temp_token_account.key,
+      Some(&pda),
+      spl_token::instruction::AuthorityType::AccountOwner,
+      initializer.key,
+      &[&initializer.key],
+    )?;
+
+    msg!("Calling the token program to transfer token account ownership...");
+    invoke(
+      &owner_change_ix,
+      &[
+          temp_token_account.clone(),
+          initializer.clone(),
+          token_program.clone(),
+      ],
+    )?;
     Ok(())
   }
 }
